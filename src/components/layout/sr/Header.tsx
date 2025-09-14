d8c376a5f2beabbc0ee466d9ef1474ad7beccb07
Zatezalo <zatezalo123@gmail.com>
2025-09-14 19:06:06 +0200

    [App] Update header text to reflect correct team member name for accuracy

--- src/components/layout/sr/Header.tsx
+++ src/components/layout/sr/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
             <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
               <Users className="w-5 h-5 text-primary-foreground" />
             </div>
-            <span className="text-foreground">Tatjana Tim Transformer</span>
+            <span className="text-foreground">Tatjana Šokčić</span>
           </Link>
 
           {/* Desktop Navigation */}
